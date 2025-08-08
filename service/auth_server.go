0a47e38f5c56d2d4aa6884c3efeab184273b88b7
phamnam2003 <namphamhai7@gmail.com>
2025-08-08 11:39:29 +0700

    feat(server): imple auth server login func protobuf

---
+++ service/auth_server.go
@@ -0,0 +1,45 @@
+package service
+
+import (
+	"context"
+	"time"
+
+	"github.com/go-http-server/grpc/protoc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// AuthServer is the server API for AuthService service.
+type AuthServer struct {
+	protoc.UnimplementedAuthServiceServer
+	store AccountStore
+	maker TokenMaker
+}
+
+// NewAuthServer creates a new instance of AuthServer.
+func NewAuthServer(store AccountStore, maker TokenMaker) *AuthServer {
+	return &AuthServer{store: store, maker: maker}
+}
+
+func (s *AuthServer) Login(ctx context.Context, req *protoc.LoginRequest) (*protoc.LoginResponse, error) {
+	acc, err := s.store.Find(req.GetUsername())
+	if err != nil {
+		return nil, status.Errorf(codes.NotFound, "store: %s", err)
+	}
+
+	err = acc.IsCorrectPassword(req.GetPassword())
+	if err != nil {
+		return nil, status.Errorf(codes.Unauthenticated, "password is incorrect: %s", err)
+	}
+
+	token, err := s.maker.CreateToken(acc, 5*time.Second)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "camnnot create token: %s", err)
+	}
+
+	res := &protoc.LoginResponse{
+		AccessToken: token,
+	}
+
+	return res, nil
+}
