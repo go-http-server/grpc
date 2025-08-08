fccc11c77b313b1a30a08a5c9fb959dd64f07f4b
phamnam2003 <namphamhai7@gmail.com>
2025-08-08 15:56:56 +0700

    feat(interceptor): imple interceptor append out going request with metadata access token header

---
+++ client/auth_interceptor.go
@@ -0,0 +1,95 @@
+package client
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+// AuthInterceptor is a gRPC interceptor that handles authentication for incoming requests.
+type AuthInterceptor struct {
+	authClient  *AuthClient
+	authMethods map[string]bool
+	accessToken string
+}
+
+// NewAuthInterceptor creates a new AuthInterceptor instance.
+func NewAuthInterceptor(authClient *AuthClient, authMethods map[string]bool, refreshTokenDuration time.Duration) (*AuthInterceptor, error) {
+	interceptor := &AuthInterceptor{
+		authClient:  authClient,
+		authMethods: authMethods,
+	}
+
+	err := interceptor.scheduleRefreshToken(refreshTokenDuration)
+	if err != nil {
+		return nil, err
+	}
+
+	return interceptor, nil
+}
+
+// scheduleRefreshToken schedules the token refresh operation at a specified interval.
+func (interceptor *AuthInterceptor) scheduleRefreshToken(refreshTokenDuration time.Duration) error {
+	err := interceptor.refreshToken()
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		wait := refreshTokenDuration
+		for {
+			time.Sleep(wait)
+			err := interceptor.refreshToken()
+			if err != nil {
+				wait = time.Second
+			} else {
+				wait = refreshTokenDuration
+			}
+		}
+	}()
+
+	return nil
+}
+
+// refreshToken refreshes the access token by calling the auth client.
+func (interceptor *AuthInterceptor) refreshToken() error {
+	accessToken, err := interceptor.authClient.Login()
+	if err != nil {
+		return err
+	}
+
+	interceptor.accessToken = accessToken
+	log.Printf("Access token refreshed: %s", accessToken)
+	return nil
+}
+
+// Unary intercepts unary RPCs to add authentication headers.
+func (interceptor *AuthInterceptor) Unary() grpc.UnaryClientInterceptor {
+	return func(ctx context.Context, method string, req, reply any, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+		log.Printf("Intercepting unary RPC: %s", method)
+		if interceptor.authMethods[method] {
+			return invoker(interceptor.attachToken(ctx), method, req, reply, cc, opts...)
+		}
+
+		return invoker(ctx, method, req, reply, cc, opts...)
+	}
+}
+
+func (interceptor *AuthInterceptor) Stream() grpc.StreamClientInterceptor {
+	return func(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string, streamer grpc.Streamer, opts ...grpc.CallOption) (grpc.ClientStream, error) {
+		log.Printf("Intercepting client stream RPC: %s", method)
+
+		if interceptor.authMethods[method] {
+			return streamer(interceptor.attachToken(ctx), desc, cc, method, opts...)
+		}
+
+		return streamer(ctx, desc, cc, method, opts...)
+	}
+}
+
+func (interceptor *AuthInterceptor) attachToken(ctx context.Context) context.Context {
+	return metadata.AppendToOutgoingContext(ctx, "authorization", interceptor.accessToken)
+}
