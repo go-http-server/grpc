ebbd4a25ffccdf14a61cc69c05033827514c047b
phamnam2003 <namphamhai7@gmail.com>
2025-08-12 14:19:46 +0700

    feat(rpc): imple rpc func from server and service client

--- client/route_guide_client.go
+++ client/route_guide_client.go
@@ -2,6 +2,8 @@ package client
 
 import (
 	"context"
+	"io"
+	"log"
 	"time"
 
 	"github.com/go-http-server/grpc/protoc"
@@ -28,4 +30,32 @@ func (rgCli *RouteGuideClient) GetFeature(point *protoc.Point) (*protoc.Feature,
 	}
 
 	return feature, nil
+}
+
+func (rgCli *RouteGuideClient) ListFeatures(rectangle *protoc.Rectangle) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	stream, err := rgCli.service.ListFeatures(ctx, rectangle)
+	if err != nil {
+		return err
+	}
+
+	for {
+		// infinite loop to receive features from stream server
+		feature, err := stream.Recv()
+		if err == io.EOF {
+			// end of the stream
+			break
+		}
+
+		if err != nil {
+			return err
+		}
+
+		log.Printf("Feature: name: %q, point:(%v, %v)", feature.GetName(),
+			feature.GetLocation().GetLatitude(), feature.GetLocation().GetLongitude())
+	}
+
+	return nil
 }
--- service/route_guide_server.go
+++ service/route_guide_server.go
@@ -3,9 +3,13 @@ package service
 import (
 	"context"
 	"encoding/json"
+	"math"
 	"os"
 
 	"github.com/go-http-server/grpc/protoc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -30,6 +34,17 @@ func (s *RouteGuideServer) loadFeatures(filename string) error {
 	return nil
 }
 
+// NewRouteGuideServer creates a new instance of RouteGuideServer and loads features from a JSON file.
+func NewRouteGuideServer() (*RouteGuideServer, error) {
+	s := &RouteGuideServer{}
+
+	err := s.loadFeatures("./sample/route_guide.json")
+	if err != nil {
+		return nil, err
+	}
+	return s, nil
+}
+
 // GetFeature retrieves the feature at the given point and implements the GetFeature method of the RouteGuideServer interface.
 func (s *RouteGuideServer) GetFeature(ctx context.Context, point *protoc.Point) (*protoc.Feature, error) {
 	err := contextError(ctx)
@@ -48,13 +63,35 @@ func (s *RouteGuideServer) GetFeature(ctx context.Context, point *protoc.Point) 
 	return &protoc.Feature{Location: point}, nil
 }
 
-// NewRouteGuideServer creates a new instance of RouteGuideServer and loads features from a JSON file.
-func NewRouteGuideServer() (*RouteGuideServer, error) {
-	s := &RouteGuideServer{}
+// inRange checks if a point is within the given rectangle.
+func inRange(point *protoc.Point, rect *protoc.Rectangle) bool {
+	left := math.Min(float64(rect.Lo.Longitude), float64(rect.Hi.Longitude))
+	right := math.Max(float64(rect.Lo.Longitude), float64(rect.Hi.Longitude))
+	top := math.Max(float64(rect.Lo.Latitude), float64(rect.Hi.Latitude))
+	bottom := math.Min(float64(rect.Lo.Latitude), float64(rect.Hi.Latitude))
 
-	err := s.loadFeatures("./sample/route_guide.json")
-	if err != nil {
-		return nil, err
+	if float64(point.Longitude) >= left &&
+		float64(point.Longitude) <= right &&
+		float64(point.Latitude) >= bottom &&
+		float64(point.Latitude) <= top {
+		return true
 	}
-	return s, nil
+	return false
+}
+
+func (s *RouteGuideServer) ListFeatures(req *protoc.Rectangle, streaming grpc.ServerStreamingServer[protoc.Feature]) error {
+	for _, feature := range s.savedFeatures {
+		err := contextError(streaming.Context())
+		if err != nil {
+			return err
+		}
+
+		if inRange(feature.Location, req) {
+			if err := streaming.Send(feature); err != nil {
+				return status.Errorf(codes.Internal, "failed to send feature: %v", err)
+			}
+		}
+	}
+
+	return nil
 }
